4d144cbfd1766b0e6106bb94a7ccb7cd3d1fae4b
haimeurz <haimeurz@gmail.com>
2024-02-03 18:02:18 -0500

    change test so its simulate a game where you lose 3 lives

--- src/default-test/java/nl/tudelft/jpacman/LauncherSmokeTest.java
+++ src/default-test/java/nl/tudelft/jpacman/LauncherSmokeTest.java
@@ -53,6 +53,52 @@ public class LauncherSmokeTest {
      * @throws InterruptedException Since we're sleeping in this test.
      */
     @SuppressWarnings({"magicnumber", "methodlength", "PMD.JUnitTestContainsTooManyAsserts"})
+//    @Test
+//    void smokeTest() throws InterruptedException {
+//        Game game = launcher.getGame();
+//        Player player = game.getPlayers().get(0);
+//
+//        // start cleanly.
+//        assertThat(game.isInProgress()).isFalse();
+//        game.start();
+//        assertThat(game.isInProgress()).isTrue();
+//        assertThat(player.getScore()).isZero();
+//
+//        // get points
+//        game.move(player, Direction.EAST);
+//        assertThat(player.getScore()).isEqualTo(10);
+//
+//        // now moving back does not change the score
+//        game.move(player, Direction.WEST);
+//        assertThat(player.getScore()).isEqualTo(10);
+//
+//        // try to move as far as we can
+//        move(game, Direction.EAST, 7);
+//        assertThat(player.getScore()).isEqualTo(60);
+//
+//        // move towards the monsters
+//        move(game, Direction.NORTH, 6);
+//        assertThat(player.getScore()).isEqualTo(120);
+//
+//        // no more points to earn here.
+//        move(game, Direction.WEST, 2);
+//        assertThat(player.getScore()).isEqualTo(120);
+//
+//        move(game, Direction.NORTH, 2);
+//
+//        // Sleeping in tests is generally a bad idea.
+//        // Here we do it just to let the monsters move.
+//        Thread.sleep(500L);
+//
+//        // we're close to monsters, this will get us killed.
+//        move(game, Direction.WEST, 10);
+//        move(game, Direction.EAST, 10);
+//        assertThat(player.isAlive()).isFalse();
+//
+//        game.stop();
+//        assertThat(game.isInProgress()).isFalse();
+//    }
+
     @Test
     void smokeTest() throws InterruptedException {
         Game game = launcher.getGame();
@@ -63,6 +109,7 @@ public class LauncherSmokeTest {
         game.start();
         assertThat(game.isInProgress()).isTrue();
         assertThat(player.getScore()).isZero();
+        assertThat(player.getLives()).isEqualTo(3);  // Verify Pacman starts with 3 lives
 
         // get points
         game.move(player, Direction.EAST);
@@ -90,14 +137,20 @@ public class LauncherSmokeTest {
         // Here we do it just to let the monsters move.
         Thread.sleep(500L);
 
-        // we're close to monsters, this will get us killed.
+        // we're close to monsters, this will get us "killed" but game continues as Pacman has more lives.
         move(game, Direction.WEST, 10);
         move(game, Direction.EAST, 10);
-        assertThat(player.isAlive()).isFalse();
+        assertThat(player.isAlive()).isTrue();  // Pacman should still be alive as he has multiple lives
+        assertThat(player.getLives()).isLessThan(3);  // Verify Pacman has lost a life
+        player.loseLife();  // Simulate Pacman losing a life
+        assertThat(player.getLives()).isEqualTo(1);  // Verify Pacman has 2 lives left
+        player.loseLife();  // Simulate Pacman losing a life
+        assertThat(player.getLives()).isEqualTo(0);  // Verify Pacman has 1 life left
 
         game.stop();
         assertThat(game.isInProgress()).isFalse();
     }
+
 
     /**
      * Make number of moves in given direction.
