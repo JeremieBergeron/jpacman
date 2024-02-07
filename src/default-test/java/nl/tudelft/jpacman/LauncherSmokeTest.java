2a35fa5f371146ed81277ed040e065bbfdb7dce2
haimeurz <haimeurz@gmail.com>
2024-02-06 21:09:42 -0500

    removed commented test

--- src/default-test/java/nl/tudelft/jpacman/LauncherSmokeTest.java
+++ src/default-test/java/nl/tudelft/jpacman/LauncherSmokeTest.java
@@ -52,53 +52,6 @@ public class LauncherSmokeTest {
      *
      * @throws InterruptedException Since we're sleeping in this test.
      */
-    @SuppressWarnings({"magicnumber", "methodlength", "PMD.JUnitTestContainsTooManyAsserts"})
-//    @Test
-//    void smokeTest() throws InterruptedException {
-//        Game game = launcher.getGame();
-//        Player player = game.getPlayers().get(0);
-//
-//        // start cleanly.
-//        assertThat(game.isInProgress()).isFalse();
-//        game.start();
-//        assertThat(game.isInProgress()).isTrue();
-//        assertThat(player.getScore()).isZero();
-//
-//        // get points
-//        game.move(player, Direction.EAST);
-//        assertThat(player.getScore()).isEqualTo(10);
-//
-//        // now moving back does not change the score
-//        game.move(player, Direction.WEST);
-//        assertThat(player.getScore()).isEqualTo(10);
-//
-//        // try to move as far as we can
-//        move(game, Direction.EAST, 7);
-//        assertThat(player.getScore()).isEqualTo(60);
-//
-//        // move towards the monsters
-//        move(game, Direction.NORTH, 6);
-//        assertThat(player.getScore()).isEqualTo(120);
-//
-//        // no more points to earn here.
-//        move(game, Direction.WEST, 2);
-//        assertThat(player.getScore()).isEqualTo(120);
-//
-//        move(game, Direction.NORTH, 2);
-//
-//        // Sleeping in tests is generally a bad idea.
-//        // Here we do it just to let the monsters move.
-//        Thread.sleep(500L);
-//
-//        // we're close to monsters, this will get us killed.
-//        move(game, Direction.WEST, 10);
-//        move(game, Direction.EAST, 10);
-//        assertThat(player.isAlive()).isFalse();
-//
-//        game.stop();
-//        assertThat(game.isInProgress()).isFalse();
-//    }
-
     @Test
     void smokeTest() throws InterruptedException {
         Game game = launcher.getGame();
